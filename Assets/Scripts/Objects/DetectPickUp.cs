8f0450da563223093fc29cfa67eaae273ec507b3
Infinityworld13 <teresita.becerra@mail.utoronto.ca>
2023-10-02 15:25:06 -0400

    Added PushObjects script
    Added matching tags and a crosshairs for LightObjects

--- Assets/Scripts/Objects/DetectPickUp.cs
+++ Assets/Scripts/Objects/DetectPickUp.cs
@@ -1,51 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PickUpController : MonoBehaviour
+public class DetectPickUp : MonoBehaviour
 {
     [Header("Pickup Settings")]
     [SerializeField] Transform holdArea;
     private GameObject heldObj;
     private Rigidbody heldObjRB;
+    [SerializeField] Image crossHairs;
+    [SerializeField] Sprite objectDetected;
+    [SerializeField] Sprite noObjectDetected;
     //[SerializeField] public LayerMask layersToHit;
-    
+
 
     [Header("Physics Parameters")]
     [SerializeField] private float pickupRange = 5.0f;
     [SerializeField] private float pickupForce = 150.0f;
 
-    // Update is called once per frame
-    private void Update()
+// Update is called once per frame
+void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // If button was clicked
+    //Shoot out a ray every frame
+    RaycastHit hit;
+    NotDetected();
+    
+    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange) 
+        && (hit.transform.gameObject.tag == "LightObj"))
+    {
+            //WE ARE HITTING THE A MOVABLE OBJECT
+        Detected();
+        
+        if(Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Clicked sth");
             if (heldObj == null)
             {
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
-                {
-                    PickupObject(hit.transform.gameObject);
-                    Debug.Log("Picked up Object");
-                    
-                }
+                PickupObject(hit.transform.gameObject);
+                Debug.Log("Picked up Object");
             }
             else
             {
                 DropObject();
             }
         }
-        if(heldObj != null)
-        {
-            MoveObject();
-        }
-        
     }
+    
+    if (heldObj != null)
+    {
+        MoveObject();
+    }
+
+}
 
     void MoveObject()
     {
-        if(Vector3.Distance(heldObj.transform.position, holdArea.position) > 0.1f)
+        if (Vector3.Distance(heldObj.transform.position, holdArea.position) > 0.1f)
         {
             Vector3 moveDirection = (holdArea.position - heldObj.transform.position);
             heldObjRB.AddForce(moveDirection * pickupForce);
@@ -68,11 +78,21 @@ public class PickUpController : MonoBehaviour
 
     void DropObject()
     {
-            heldObjRB.useGravity = true;
-            heldObjRB.drag = 1;
-            heldObjRB.constraints = RigidbodyConstraints.None;
+        heldObjRB.useGravity = true;
+        heldObjRB.drag = 1;
+        heldObjRB.constraints = RigidbodyConstraints.None;
 
-            heldObjRB.transform.parent = null;
-            heldObj = null;
+        heldObjRB.transform.parent = null;
+        heldObj = null;
+    }
+
+    void Detected()
+    {
+        crossHairs.sprite = objectDetected;
+    }
+
+    void NotDetected()
+    {
+        crossHairs.sprite = noObjectDetected;
     }
 }
