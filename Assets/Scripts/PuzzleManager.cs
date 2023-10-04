2f48b8b77b06ae3890f9a6d7534a2114ace28ade
Leen Al Lababidi <leenlab2@gmail.com>
2023-10-04 11:18:02 -0400

    Added puzzle manager with events for KeyItemPlaced and PuzzleComplete

---
+++ Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    private List<ObjectDistance> keyItems = new List<ObjectDistance>();
+
+    public delegate void PuzzleComplete();
+    public event PuzzleComplete OnPuzzleComplete;
+
+    private void Awake()
+    {
+        keyItems = FindObjectsOfType<ObjectDistance>().ToList();
+        foreach (var item in keyItems)
+        {
+            item.OnKeyItemPlaced += HandleKeyItemPlaced;
+        }
+    }
+
+    private void HandleKeyItemPlaced(GameObject sender)
+    {
+        keyItems.Remove(sender.GetComponent<ObjectDistance>());
+        if (keyItems.Count == 0)
+        {
+            Debug.Log("Puzzle complete!");
+            OnPuzzleComplete();
+        }
+    }
+}
