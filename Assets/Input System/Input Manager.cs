a5add2a63086c5a78b34a502f83154bbb89370b3
valeriewirianto <valerie.wirianto@mail.utoronto.ca>
2023-10-27 15:22:50 -0400

    revert to working Input Manager

--- Assets/Input System/Input Manager.cs
+++ Assets/Input System/Input Manager.cs
@@ -54,20 +54,6 @@ public class InputManager : MonoBehaviour
         // Placement Input Map
         playerInputActions.Placement.Place.performed += ObjectPlace;
     }
-
-    private void OnApplicationFocus(bool focus) {
-        if (focus)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
-        }
-    }
-
-    #region Player Movement
-
     private void FixedUpdate()
     {
         MovePlayer();
@@ -109,7 +95,7 @@ public class InputManager : MonoBehaviour
         Vector2 cameraInput = playerInputActions.Player.Look.ReadValue<Vector2>();
 
         // Move the player to look around left/right when mouse pans left/right
-        transform.Rotate(0, cameraInput.x * _mouseSensitivity * 1.2f, 0);
+        transform.Rotate(0, cameraInput.x * _mouseSensitivity, 0);
 
         // Move the camera to look around up/down when mouse pans up/down
         Transform playerCamera = GetComponentInChildren<Camera>().transform;
@@ -128,8 +114,6 @@ public class InputManager : MonoBehaviour
         //playerCamera.Rotate(-cameraInput.y * _mouseSensitivity, 0, 0);
     }
     #endregion
-    #endregion
-    
 
     #region Television Toggle
     private void OpenTelevision(InputAction.CallbackContext obj)
