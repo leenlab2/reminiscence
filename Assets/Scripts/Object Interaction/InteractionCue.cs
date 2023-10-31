60977595cc90c49a62fbd707e6d37b49e4c3cde6
Tianyu Zhang <tianyuzhang0704@outlook.com>
2023-10-31 00:23:06 -0400

    Simplify Text

--- Assets/Scripts/Object Interaction/InteractionCue.cs
+++ Assets/Scripts/Object Interaction/InteractionCue.cs
@@ -17,12 +17,14 @@ public enum InteractionCueType
 public class InteractionCue : MonoBehaviour
 {
     private TMP_Text _interactText;
+    private TMP_Text _pickupText;
 
-    private string pickUp = "Left click to pick up";
-    private string insertTape = "Left click to insert tape";
-    private string removeTape = "Left click to remove tape";
-    private string holdText = "Hold left to aim and click right to place. Press E to inspect.";
-    private string inspectionText = "Use mouse to rotate object. Press E to exit inspection.";
+    private string pickUp = "[LeftClick] Pickup";
+    private string insertTape = "[LeftClick] insert tape";
+    private string removeTape = "[LeftClick to remove";
+    private string putDown = "[E] to inspect";
+    private string holdText = "[Hold LeftClick + RightClick] Put down";
+    private string inspectionText = "[Mouse] to rotate              [E] to exit";
     private string empty = "";
 
     // Start is called before the first frame update
@@ -30,6 +32,7 @@ public class InteractionCue : MonoBehaviour
     {
         Debug.Log("InteractionCue");
         _interactText = GameObject.Find("Interact Text").GetComponent<TMP_Text>();
+        _pickupText = GameObject.Find("Pickup Text").GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
@@ -45,18 +48,21 @@ public class InteractionCue : MonoBehaviour
             _interactText.text = empty;
         } else if (type == InteractionCueType.Pickup)
         {
-            _interactText.text = pickUp;
+            _pickupText.text = pickUp;
         } else if (type == InteractionCueType.InsertTape)
         {
-            _interactText.text = insertTape;
+            _pickupText.text = insertTape;
+            _interactText.text = empty;
         } else if (type == InteractionCueType.RemoveTape)
         {
-            _interactText.text = removeTape;
+            _pickupText.text = removeTape;
         } else if (type == InteractionCueType.Hold)
         {
+            _pickupText.text = putDown;
             _interactText.text = holdText;
         } else if (type == InteractionCueType.Inspection)
         {
+            _pickupText.text = empty;
             _interactText.text = inspectionText;
         }
     }
